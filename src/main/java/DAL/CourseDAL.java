32edb9d7df346551bf02095a4c960037efbdf00b
Trương Thành Ä?ại <thanhdaibl1572002@gmail.com>
2023-02-19 16:51:40 +0700

    Update Utils folder
    - Created Connection class
    - Created Validate class
    - Edit CourseDAL class

--- src/main/java/DAL/CourseDAL.java
+++ src/main/java/DAL/CourseDAL.java
@@ -1,12 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package DAL;
 
 import DTO.Course;
-import java.sql.Connection;
-import java.sql.DriverManager;
+import Utils.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -14,43 +9,18 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author nakng
- */
 public class CourseDAL {
-    private Connection con;
     private static String sql;
     private static Statement stm;
-    private static ResultSet rs;
-    public boolean openConnection(){
-        try {
-            String dbPath = "jdbc:mysql://localhost: 3306/school?useUnicode=yes&characterEncoding=UTF-8";
-            con = DriverManager.getConnection(dbPath, "root", "");
-            return true;
-        } catch (SQLException ex) {
-            Logger.getLogger(CourseDAL.class.getName()).log(Level.SEVERE, null, ex);
-            return false;
-        }
-    }
-    
-    public void closeConnection(){
-        if(con!=null){
-            try {
-                con.close();
-            } catch (SQLException ex) {
-                Logger.getLogger(CourseDAL.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-    
-    }
+    private static ResultSet rs;  
+    private static final Connection connection = new Connection();
     
     public ArrayList<Course> getAllCourse(){
-        ArrayList<Course> list = new ArrayList<Course>();
-        if(openConnection()){
+        ArrayList<Course> list = new ArrayList<>();
+        if(connection.openConnection()){
             try {
                 sql = "select * from course";
-                stm = con.createStatement();
+                stm = connection.con.createStatement();
                 rs = stm.executeQuery(sql);
                 while(rs.next()){
                     Course acc = new Course();
@@ -63,7 +33,7 @@ public class CourseDAL {
             } catch (SQLException ex) {
                 Logger.getLogger(CourseDAL.class.getName()).log(Level.SEVERE, null, ex);
             }finally{
-                closeConnection();
+                connection.closeConnection();
             }
         }   
         return list;
---
+++ src/main/java/Utils/Connection.java
@@ -0,0 +1,32 @@
+package Utils;
+
+import DAL.CourseDAL;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Connection {
+    public java.sql.Connection con;  
+    
+    public boolean openConnection(){
+        try {
+            String dbPath = "jdbc:mysql://localhost: 3306/school?useUnicode=yes&characterEncoding=UTF-8";
+            con = DriverManager.getConnection(dbPath, "root", "");
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(CourseDAL.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+    
+    public void closeConnection(){
+        if(con!=null){
+            try {
+                con.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(CourseDAL.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
